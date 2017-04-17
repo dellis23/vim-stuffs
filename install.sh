51a4d0fb6eafa614d13e22768b3d4370a407f20d
Daniel Ellis <ellisd23@gmail.com>
2017-04-17 21:49:30 +0000

    Swapfiles and install.

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s $(readlink -f .vim) ~/.vim
 ln -s $(readlink -f .vimrc) ~/.vimrc
 ln -s $(readlink -f .bash_profile) ~/.bash_profile
 ln -s $(readlink -f .git-completion.bash) ~/.git-completion.bash
+git config --global core.excludesfile $(readlink -f .gitignore)
