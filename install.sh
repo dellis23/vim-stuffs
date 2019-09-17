87347c096e200e4830e6895ffedc92987fa736f6
Daniel Ellis <danielellis@google.com>
2019-09-17 00:11:58 +0000

    Force screen to use login shell.

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 ln -s $(readlink -f .vim) ~/.vim
 ln -s $(readlink -f .vimrc) ~/.vimrc
 ln -s $(readlink -f .bash_profile) ~/.bash_profile
+ln -s $(readlink -f .screenrc) ~/.screenrc
 ln -s $(readlink -f .git-completion.bash) ~/.git-completion.bash
 git config --global core.excludesfile $(readlink -f .gitignore)
