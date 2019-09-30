bf8799b3a739584fd754ec6e203409e9da01cfab
Daniel Ellis <danielellis@google.com>
2019-09-30 16:10:17 -0700

    Add pdb aliases.

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-ln -s $(readlink -f .vim) ~/.vim
+if [ -d "$HOME/.vim" ]; then
+    echo "$HOME/.vim already exists.  Skipping!"
+else
+    ln -s $(readlink -f .vim) ~/.vim
+fi
 ln -s $(readlink -f .vimrc) ~/.vimrc
+ln -s $(readlink -f .pdbrc) ~/.pdbrc
 ln -s $(readlink -f .bash_profile) ~/.bash_profile
 ln -s $(readlink -f .screenrc) ~/.screenrc
 ln -s $(readlink -f .git-completion.bash) ~/.git-completion.bash
