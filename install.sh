d1ab1bbe428793e62129383f63347b25760f9c32
Daniel Ellis <ellisd23@gmail.com>
2019-10-13 16:11:14 -0700

    Fix install.sh on mac.

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+READLINK=readlink
+if [ "$(uname)" = "Darwin" ]; then
+    READLINK=greadlink
+fi
+which -s $READLINK || { echo "Couldn't find an appropriate readlink.  If you're on mac, try 'brew install greadlink'." ; exit 1; }
+
 if [ -d "$HOME/.vim" ]; then
     echo "$HOME/.vim already exists.  Skipping!"
 else
-    ln -s $(readlink -f .vim) ~/.vim
+    ln -s $($READLINK -f .vim) ~/.vim
 fi
-ln -s $(readlink -f .vimrc) ~/.vimrc
-ln -s $(readlink -f .pdbrc) ~/.pdbrc
-ln -s $(readlink -f .bash_profile) ~/.bash_profile
-ln -s $(readlink -f .screenrc) ~/.screenrc
-ln -s $(readlink -f .git-completion.bash) ~/.git-completion.bash
-git config --global core.excludesfile $(readlink -f .gitignore)
+ln -s $($READLINK -f .vimrc) ~/.vimrc
+ln -s $($READLINK -f .pdbrc) ~/.pdbrc
+ln -s $($READLINK -f .bash_profile) ~/.bash_profile
+ln -s $($READLINK -f .screenrc) ~/.screenrc
+ln -s $($READLINK -f .git-completion.bash) ~/.git-completion.bash
+git config --global core.excludesfile $($READLINK -f .gitignore)
